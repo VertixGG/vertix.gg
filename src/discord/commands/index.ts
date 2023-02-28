fa57aafa7844693df5a51eca32cc641acf3bf2dd
Leonid Vinikov <czf.leo123@gmail.com>
2023-02-28 21:04:28 +0200

    New: Commands - Setup command.

--- src/discord/commands/index.ts
+++
@@ -1,9 +0,0 @@
-import { ICommand } from "../interfaces/command";
-
-import { Hello } from "./hello";
-import { HelloEmbed } from "./hello-embed";
-
-export const Commands: ICommand[] = [
-    Hello,
-    HelloEmbed,
-];

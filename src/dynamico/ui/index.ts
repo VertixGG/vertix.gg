826cd68858a878777cb04feb5908095a21d9e4dd
Leonid Vinikov <czf.leo123@gmail.com>
2023-03-18 17:22:28 +0200

    Internal: Register UI - Move to index

---
+++ src/dynamico/ui/index.ts
@@ -0,0 +1,6 @@
+export { EditDynamicChannel } from "./edit-dynamic-channel/edit-dynamic-channel";
+export { EditUsersPermissions } from "./edit-users-permissions/edit-users-permissions";
+export { GlobalResponse } from "./global-responses/global-responses";
+export { NotifyMaxMasterChannels } from  "./notify-max-master-channels";
+export { NotifyPermissions } from  "./notify-permissions";
+export { NotifySetupSuccess } from  "./notify-setup-success";

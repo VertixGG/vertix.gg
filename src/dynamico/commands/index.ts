095f9ecffed4fd3e925e27546bbecd289d097316
Leonid Vinikov <czf.leo123@gmail.com>
2023-04-15 11:24:54 +0300

    New: DMManager - Add embed sender

--- src/dynamico/commands/index.ts
+++ src/dynamico/commands/index.ts
@@ -2,14 +2,12 @@ import { ICommand } from "@dynamico/interfaces/command";
 
 import { Config } from "@dynamico/commands/config";
 import { Setup } from "@dynamico/commands/setup";
-import { Welcome } from "@dynamico/commands/welcome";
 
 import Logger from "@internal/modules/logger";
 
 export const Commands: ICommand[] = [
     Config,
     Setup,
-    Welcome,
 ];
 
 export const commandsLogger = new class CommandsLoggers extends Logger {

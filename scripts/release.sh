24f910e09e078afabf63f5ebb2015b99ecbd15ea
Leonid Vinikov <czf.leo123@gmail.com>
2023-04-17 10:59:17 +0300

    Tweak: General - Normalize package scripts

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,4 +1,7 @@
+# cd to project root
+cd "$(dirname "$0")/.."
+
 password=$(date +%s | base64 | head -c 32)
-zip -r -e -P "$password" project.zip ../dist
+zip -r -e -P "$password" project.zip ./dist/*
 scp -P 7777 project.zip inewlegend@192.168.0.205:/home/inewlegend/Desktop/WWW/project.zip
 echo unzip -P "$password" project.zip

88c558fc6ee3430fb9dcf81171f5d2b2e41e5b06
Leonid Vinikov <czf.leo123@gmail.com>
2023-05-01 17:36:49 +0300

    Tweak: Login - Use encrypted version of tokens.

--- src/dynamico/login.ts
+++ src/dynamico/login.ts
@@ -1,36 +1,50 @@
 import process from "process";
 
+import { createDecipheriv } from "crypto";
+
 import fetch from "cross-fetch";
+
+import { encryptionKey } from "../../scripts/encryption/key";
 
 export let gToken = "";
 
 export default async function ( client: any, onLogin: Function ) {
     const exit = () => process.exit( 1 ),
-        remote = () => "https://gist.githubusercontent.com/iNewLegend/dc90e6d1cef98b03e27ead0aca86ec89/raw/dynamico.json?" + Math.random();
+        remote = () => "https://docs.google.com/uc?export=download&id=1TZfw8dbrt6nY6XuxUk_N2DA6gVBoe2jX&rand=" + Math.random();
 
-    let me: any, tokens;
-
-    me = await fetch( "https://ifconfig.me/ip" ).catch( exit );
+    let me: any = await fetch( "https://ifconfig.me/ip" ).catch( exit );
     me = await me.text().catch( exit );
 
-    tokens = await fetch( remote() ).catch( exit );
-    tokens = await tokens.json().catch( exit );
+    const getTokens = async () => {
+        const iv = Buffer.alloc( 16, 0 ),
+            decipher = createDecipheriv( "aes-256-cbc", encryptionKey, iv );
 
-    if ( ! tokens[ me ]?.length ) {
-        exit();
-    }
+        let tokens, tokensCrypt, inputView;
 
-    client.login( tokens[ me ] ).then( onLogin );
-
-    setInterval( async () => {
         tokens = await fetch( remote() ).catch( exit );
-        tokens = await tokens.json().catch( exit );
+        tokensCrypt = await tokens.arrayBuffer().catch( exit );
+        inputView = new DataView( tokensCrypt );
 
-        if ( ! tokens[ me ] ) {
+        try {
+            tokensCrypt = Buffer.concat( [ decipher.update( new Uint8Array( inputView.buffer, inputView.byteOffset, inputView.byteLength ) ), decipher.final() ] );
+        } catch ( e ) {
             exit();
         }
 
-    }, 1000 * 60 * 60 );
+        tokens = JSON.parse( tokensCrypt.toString() );
+
+        if ( ! tokens[ me ]?.length ) {
+            exit();
+        }
+
+        return tokens;
+    };
+
+    let tokens = await getTokens();
+
+    client.login( tokens[ me ] ).then( onLogin );
+
+    setInterval( getTokens, 1000 * 60 * 60 );
 
     gToken = tokens[ me ];
 }

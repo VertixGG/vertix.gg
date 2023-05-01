4b51f9e575cc8b9880b0c18afa8842f2cc19d5c1
Leonid Vinikov <czf.leo123@gmail.com>
2023-05-01 17:22:17 +0300

    Fix: Scripts - Passwords were too big, which made line collapse.

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,7 +1,7 @@
 # cd to project root
 cd "$(dirname "$0")/.."
 
-password=$(date +%s | openssl rand -base64 64)
+password=$(date +%s | openssl rand -base64 32)
 zip -r -e -P "$password" project.zip ./dist/*
 scp -P 7777 project.zip inewlegend@192.168.0.205:/home/inewlegend/Desktop/WWW/project.zip
 echo unzip -P "$password" project.zip

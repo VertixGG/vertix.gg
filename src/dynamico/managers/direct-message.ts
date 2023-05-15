aed157935feb24afe5214b2c24ffea4194a09b47
Leonid Vinikov <czf.leo123@gmail.com>
2023-05-15 16:20:03 +0300

    New: DMManager - Add `!edit_embed` command.

--- src/dynamico/managers/direct-message.ts
+++ src/dynamico/managers/direct-message.ts
@@ -2,14 +2,16 @@ import fetch from "cross-fetch";
 
 import { EmbedBuilder, Guild, Message, MessageCreateOptions, TextBasedChannel } from "discord.js";
 
-import {
-    DYNAMICO_DEFAULT_COLOR_BRAND,
-    DYNAMICO_OWNERS_IDS
-} from "@dynamico/constants/dynamico";
+import { DYNAMICO_DEFAULT_COLOR_BRAND, DYNAMICO_OWNERS_IDS } from "@dynamico/constants/dynamico";
 
 import { DynamicoManager } from "@dynamico/managers/dynamico";
 
 import InitializeBase from "@internal/bases/initialize-base";
+
+const OWNER_COMMAND_SYNTAX = {
+    embed: "!embed <#channel_id> <https://message_url.com>",
+    edit_embed: "!edit_embed <#channel_id> <message_id> <https://message_url.com>"
+};
 
 export class DirectMessageManager extends InitializeBase {
     private static instance: DirectMessageManager;
@@ -45,63 +47,82 @@ export class DirectMessageManager extends InitializeBase {
             .filter( ( entry ) => entry.length );
 
         switch ( command[ 0 ] ) {
-            case "!embed": {
-                if ( command.length < 2 || command.length > 3 || ! command[ 1 ]?.length || ! command[ 2 ]?.length ) {
-                    return await message.reply( "Syntax: !embed <#channel_id> <https://message_url.com>" );
-                }
+            case "!embed":
+                await this.handleEmbedCommand( command, message );
+                break;
 
-                const channel = await DynamicoManager.$.getClient()?.channels.fetch( command[ 1 ] );
+            case "!edit_embed":
+                await this.handleEditEmbedCommand( command, message );
+                break;
 
-                if ( channel && channel.isTextBased() ) {
-                    try {
-                        const request = fetch( command[ 2 ] ),
-                            response = await request.then( async ( _response ) => {
-                                if ( ! _response.ok ) {
-                                    throw _response;
-                                }
+            default:
+                let syntaxMessage = "Available commands:\n\n";
 
-                                return _response.json();
-                            } );
+                Object.values( OWNER_COMMAND_SYNTAX ).forEach(
+                    ( syntax ) => syntaxMessage += `${ syntax }\n`
+                );
 
-                        const embedBuilder = new EmbedBuilder();
-
-                        if ( response.title ) {
-                            embedBuilder.setTitle( response.title );
-                        }
-
-                        if ( response.thumbnail ) {
-                            embedBuilder.setThumbnail( response.thumbnail );
-                        }
-
-                        if ( response.image ) {
-                            embedBuilder.setImage( response.image );
-                        }
-
-                        if ( response.description ) {
-                            embedBuilder.setDescription( response.description );
-                        }
-
-                        if ( response.color ) {
-                            embedBuilder.setColor( parseInt( response.color ) );
-                        }
-
-                        ( channel as TextBasedChannel ).send( {
-                            embeds: [ embedBuilder ]
-                        } ).catch( async () => {
-                            await message.reply( "Message was not sent!" );
-                        } )
-                            .then( async () => {
-                                await message.reply( "Message sent!" );
-                            } );
-
-                    } catch ( e: any ) {
-                        await message.reply( e.message as string );
-                    }
-                } else {
-                    await message.reply( "Channel is wrong" );
-                }
-            }
+                await message.reply( syntaxMessage );
         }
+    }
+
+    private async handleEmbedCommand( command: string[], message: Message ) {
+        if ( command.length < 2 || command.length > 3 || ! command[ 1 ]?.length || ! command[ 2 ]?.length ) {
+            return await message.reply( OWNER_COMMAND_SYNTAX.embed );
+        }
+
+        const channel = await DynamicoManager.$.getClient()?.channels.fetch( command[ 1 ] );
+
+        if ( ! channel || ! channel.isTextBased() ) {
+            return await message.reply( "Invalid channel!" );
+        }
+
+        const response = await this.fetchEmbed( command[ 2 ], message );
+
+        if ( response ) {
+            await this.sendEmbedCommand( channel, response, message );
+        }
+    }
+
+    private async handleEditEmbedCommand( command: string[], message: Message ) {
+        if ( command.length < 3 || command.length > 4 || ! command[ 1 ]?.length || ! command[ 2 ]?.length || ! command[ 3 ]?.length ) {
+            return await message.reply( OWNER_COMMAND_SYNTAX.edit_embed );
+        }
+
+        const channel = await DynamicoManager.$.getClient()?.channels.fetch( command[ 1 ] );
+
+        if ( ! channel || ! channel.isTextBased() ) {
+            return await message.reply( "Invalid channel!" );
+        }
+
+        // Find message.
+        const messageToEdit = await channel.messages.fetch( command[ 2 ] ).catch( () => null );
+
+        if ( ! messageToEdit ) {
+            await message.reply( "Message not found!" );
+        }
+
+        const response = await this.fetchEmbed( command[ 3 ], message );
+
+        if ( response && messageToEdit ) {
+            await messageToEdit.edit( { embeds: [ this.buildEmbed( response ) ] } )
+                .catch( async () => {
+                    await message.reply( "Message was not edited!" );
+                } )
+                .then( async () => {
+                    await message.reply( "Message edited!" );
+                } );
+        }
+    }
+
+    private async sendEmbedCommand( channel: TextBasedChannel, response: any, message: Message ) {
+        channel.send( { embeds: [ this.buildEmbed( response ) ] } )
+            .catch( async () => {
+                await message.reply( "Message was not sent!" );
+            } )
+            .then( async () => {
+                await message.reply( "Message sent!" );
+            } );
     }
 
     public async sendLeaveMessageToOwner( guild: Guild ) {
@@ -126,4 +147,52 @@ export class DirectMessageManager extends InitializeBase {
             this.logger.error( this.sendToUser, `Failed to send message to user, userId: '${ userId }'` );
         } );
     }
+
+    private async fetchEmbed( url: string, message: Message ) {
+        let response: any;
+
+        try {
+            const request = fetch( url );
+
+            response = await request.then( async ( _response ) => {
+                if ( ! _response.ok ) {
+                    throw _response;
+                }
+
+                return _response.json();
+            } );
+        } catch ( e: any ) {
+            await message.reply( e.message as string );
+            response = null;
+        }
+
+        return response;
+    }
+
+    private buildEmbed( response: any ) {
+        const embedBuilder = new EmbedBuilder();
+
+        if ( response.title ) {
+            embedBuilder.setTitle( response.title );
+        }
+
+        if ( response.thumbnail ) {
+            embedBuilder.setThumbnail( response.thumbnail );
+        }
+
+        if ( response.image ) {
+            embedBuilder.setImage( response.image );
+        }
+
+        if ( response.description ) {
+            embedBuilder.setDescription( response.description );
+        }
+
+        if ( response.color ) {
+            embedBuilder.setColor( parseInt( response.color ) );
+        }
+
+        return embedBuilder;
+    }
+
 }

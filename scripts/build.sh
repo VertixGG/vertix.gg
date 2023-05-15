3408eb6533dbcf19b6158be1688e0ea441a09086
Leonid Vinikov <czf.leo123@gmail.com>
2023-05-15 16:00:00 +0300

    Internal Fix - Issue with building number.

--- scripts/build.sh
+++ scripts/build.sh
@@ -12,11 +12,20 @@ mkdir -p dist/prisma
 # copy schema.prisma to dist/prisma folder
 cp ./prisma/schema.prisma dist/prisma/schema.prisma
 
+# Run `update-package-version.sh` to build number
+bash ./scripts/update-package-version.sh
+
 # Copy package.json to dist folder
 cp ./package.json dist/package.json
 
 # Take all '.sh' files in `./tools/scripts-that-comes-with-build` and copy them to `./dist`
-cp ./tools/scripts-that-comes-with-build/*.sh ./dist/
+cp ./tools/scripts-that-comes-with-build/* ./dist/
+
+# Copy `package.json to `./dist`
+cp -f ./package.json ./dist/package.json
+
+# Copy `yarn.lock to `./dist`
+cp -f ./yarn.lock ./dist/yarn.lock
 
 # Apply chmod +x to them.
 chmod +x ./dist/*.sh

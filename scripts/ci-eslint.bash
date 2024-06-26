b5bad4d9ff44e4da5e6b3d4ff4b82812ee60381d
Leonid Vinikov <czf.leo123@gmail.com>
2024-06-26 07:04:21 +0300

    New: (`vertix-base`) - Add `ci-eslint.bash` script to project Added a new bash script `ci-eslint.bash` to run eslint as part of the project's CI. Updated `package.json` with a new script command `vertix:ci:eslint` to use this bash script. Changed the command used in the GitHub eslint workflow to utilize the new `vertix:ci:eslint` script for linting.

---
+++ scripts/ci-eslint.bash
@@ -0,0 +1,16 @@
+# cd to project root
+cd "$(dirname "$0")/.."
+
+# save current directory
+CURRENT_DIR=$(pwd)
+
+# create key
+cd packages/vertix-base/src/encryption/
+pwd
+bash key-gen.bash
+
+# back to root
+cd $CURRENT_DIR
+
+# run eslint
+bunx --bun eslint .

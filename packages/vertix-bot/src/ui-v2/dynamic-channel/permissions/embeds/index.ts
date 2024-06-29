adedce2b9e7067a5c86e1fe2cb89d1a06b38b6c5
Leonid Vinikov <czf.leo123@gmail.com>
2024-06-29 10:31:47 +0300

    Fix: (`vertix-bot`) - Correct file name and reference of `DynamicChannelPermissionsPrivateEmbed`
    Renamed `dynamic-channel-permissions-private-embed..ts` to `dynamic-channel-permissions-private-embed.ts` for correct naming conventions. Required an update to the export reference in `index.ts` to match the corrected filename.

--- packages/vertix-bot/src/ui-v2/dynamic-channel/permissions/embeds/index.ts
+++ packages/vertix-bot/src/ui-v2/dynamic-channel/permissions/embeds/index.ts
@@ -5,7 +5,7 @@ export { DynamicChannelPermissionsDeniedEmbed } from "./dynamic-channel-permissi
 export { DynamicChannelPermissionsGrantedEmbed } from "./dynamic-channel-permissions-granted-embed";
 export { DynamicChannelPermissionsHiddenEmbed } from "./dynamic-channel-permissions-hidden-embed";
 export { DynamicChannelPermissionsKickEmbed } from "./dynamic-channel-permissions-kick-embed";
-export { DynamicChannelPermissionsPrivateEmbed } from "./dynamic-channel-permissions-private-embed.";
+export { DynamicChannelPermissionsPrivateEmbed } from "./dynamic-channel-permissions-private-embed";
 export { DynamicChannelPermissionsPublicEmbed } from "./dynamic-channel-permissions-public-embed";
 export { DynamicChannelPermissionsShownEmbed } from "./dynamic-channel-permissions-shown-embed";
 export { DynamicChannelPermissionsUnblockedEmbed } from "./dynamic-channel-permissions-unblocked-embed";

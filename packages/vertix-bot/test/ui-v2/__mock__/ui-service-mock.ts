aa627c82a00df7227dd1c27a5399d24d091dd402
Leonid Vinikov <czf.leo123@gmail.com>
2024-06-30 11:56:46 +0300

    Tweak: (`vertix-bot`) - Override `setupCleanWorkerTimer` in `UIServiceMock`
    The method `setupCleanWorkerTimer` from `UIService` has been overridden in the `UIServiceMock` class. The new method does nothing when called, which is ideal in a testing context to avoid any unwanted side effects.

--- packages/vertix-bot/test/ui-v2/__mock__/ui-service-mock.ts
+++ packages/vertix-bot/test/ui-v2/__mock__/ui-service-mock.ts
@@ -1,6 +1,10 @@
 import UIService from "@vertix.gg/bot/src/ui-v2/ui-service";
 
 export class UIServiceMock extends UIService {
+    protected static setupCleanWorkerTimer(): void {
+        // Do nothing.
+    }
+
     public isSaveHashEnabled(): boolean {
         return false;
     }

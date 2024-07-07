727d4c39581984beba289494cbc684218b20135b
Leonid Vinikov <czf.leo123@gmail.com>
2024-07-07 05:13:04 +0300

    Remove: (`vertix-bot`) - Removed unnecessary process import and console log from `index-bun.ts` Deleted process import as it was not being extensively used. Also, the `console.log(process.env)` was deleted to clean up logging.

--- packages/vertix-bot/src/index-bun.ts
+++ packages/vertix-bot/src/index-bun.ts
@@ -1,9 +1,5 @@
-import process from "process";
-
 import { entryPoint } from "@vertix.gg/bot/src/entrypoint";
 
 Error.stackTraceLimit = Infinity;
-
-console.log(process.env);
 
 await entryPoint();

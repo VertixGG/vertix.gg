4960343ddcab4178aea032f3ab728a6c9bdc2ae9
Leonid Vinikov <czf.leo123@gmail.com>
2024-07-12 17:02:10 +0300

    New: (`vertix-test-utils`) - Add UI mock generator utility classes Added utility classes to facilitate the generation of mock UI components. This includes builders for ElementsGroup, Adapter, EmbedsGroup, Component, Element, Embed, and Modal. Each builder allows customized name, instanceTypes, components, executionSteps, and relevant items for each UI component.

---
+++ packages/vertix-test-utils/src/ui-mock-generator-util/ui-mock-generator-util-embed-builder.ts
@@ -0,0 +1,33 @@
+import { UIEmbedBase } from "@vertix.gg/gui/src/bases/ui-embed-base";
+
+import type { UIInstancesTypes } from "@vertix.gg/gui/src/bases/ui-definitions";
+
+export class UIMockGeneratorUtilEmbedBuilder {
+    private name!: string;
+    private instanceType!: UIInstancesTypes;
+
+    public withName(name: string) {
+        this.name = name;
+        return this;
+    }
+
+    public withInstanceType(instanceType: UIInstancesTypes) {
+        this.instanceType = instanceType;
+        return this;
+    }
+
+    public build() {
+        const name = this.name;
+        const instanceType = this.instanceType;
+
+        return class extends UIEmbedBase {
+            public static getName() {
+                return name;
+            }
+
+            public static getInstanceType() {
+                return instanceType;
+            }
+        };
+    }
+}

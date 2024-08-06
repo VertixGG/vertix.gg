3285ff9681618dcfe952fad619179245eb4cc8d1
Leonid Vinikov <czf.leo123@gmail.com>
2024-08-06 07:35:18 +0300

    New: (`vertix-bot`) - Add `EmojiManager` in `emoji-manager.ts`
    Introduced `EmojiManager` to handle Discord emoji functionalities. It includes methods to fetch and cache emojis from Discord API and retrieve their markdown representations. Updated dependencies in `package.json` to include `discord-api-types`.

---
+++ packages/vertix-bot/src/managers/emoji-manager.ts
@@ -0,0 +1,80 @@
+import { InitializeBase } from "@vertix.gg/base/src/bases/index";
+import { Debugger } from "@vertix.gg/base/src/modules/debugger";
+import { ServiceLocator } from "@vertix.gg/base/src/modules/service/service-locator";
+import { isDebugEnabled } from "@vertix.gg/utils/src/environment";
+
+import { gToken } from "@vertix.gg/base/src/discord/login";
+
+import { GatewayVersion } from "discord-api-types/gateway/v10";
+import { Routes } from "discord-api-types/v10";
+
+import { REST } from "discord.js";
+
+import type { RESTGetAPIApplicationEmojisResult } from "discord-api-types/v9";
+
+import type { AppService } from "@vertix.gg/bot/src/services/app-service";
+
+export class EmojiManager extends InitializeBase {
+    private static instance: EmojiManager;
+
+    private appService: AppService;
+
+    private debugger: Debugger;
+
+    private initPromise: Promise<void>;
+
+    private emojis: RESTGetAPIApplicationEmojisResult;
+
+    public static getName() {
+        return "VertixBot/Managers/Emoji";
+    }
+
+    public static get $() {
+        if ( ! EmojiManager.instance ) {
+            EmojiManager.instance = new EmojiManager();
+        }
+
+        return EmojiManager.instance;
+    }
+
+    public constructor() {
+        super( false );
+
+        this.debugger = new Debugger( this, "", isDebugEnabled( "MANAGER", EmojiManager.getName() ) );
+
+        this.initPromise = this.initialize();
+    }
+
+    protected async initialize() {
+        this.appService = await ServiceLocator.$.waitFor( "VertixBot/Services/App", {
+            silent: true,
+            timeout: 10000
+        } );
+
+        const rest = new REST({ version: GatewayVersion }).setToken(gToken);
+
+        this.emojis = await rest.get( Routes.applicationEmojis( this.appService.getClient().user.id ) ) as RESTGetAPIApplicationEmojisResult;
+
+        this.debugger.dumpDown( this.initialize, this.emojis, "emojis" );
+    }
+
+    public async getMarkdown( baseName: string, fromCache = true ) {
+        if ( ! fromCache ) {
+            await this.initialize();
+        }
+
+        await this.initPromise;
+
+        return this.getCachedMarkdown( baseName );
+    }
+
+    public getCachedMarkdown( baseName: string ) {
+        const emoji = this.emojis.items.find( emoji => emoji.name!.includes( baseName ) );
+
+        if ( emoji ) {
+            return `<:${ emoji.name }:${ emoji.id }>`;
+        }
+
+        throw new Error( `Emoji: '${ baseName }' not found` );
+    }
+}

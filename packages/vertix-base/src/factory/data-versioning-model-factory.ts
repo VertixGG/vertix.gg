88b79f9cbbb09ab71e91824fe395d9453dc1b80a
Leonid Vinikov <czf.leo123@gmail.com>
2024-08-07 07:24:00 +0300

    New: (`vertix-base`) - Add deep merge functionality in data versioning model
    Introduced deep merging of existing object values within the data versioning model. This enhancement ensures that updates incorporate existing data seamlessly using the `deepMerge` utility. Added `mergeWithExisting` method to handle the merging logic.

--- packages/vertix-base/src/factory/data-versioning-model-factory.ts
+++ packages/vertix-base/src/factory/data-versioning-model-factory.ts
@@ -2,11 +2,14 @@ import util from "node:util";
 
 import { isDebugEnabled } from "@vertix.gg/utils/src/environment";
 
+import { deepMerge } from "@vertix.gg/utils/src/object";
+
 import { ModelBaseCachedWithModel } from "@vertix.gg/base/src/bases/model-base";
 
 import { DataTypeFactory  } from "@vertix.gg/base/src/factory/data-type-factory";
 
-import type { TDefaultResult } from "@vertix.gg/base/src/factory/data-type-factory";
+import type { TDataType , TDefaultResult } from "@vertix.gg/base/src/factory/data-type-factory";
+
 import type { TBaseModelStub } from "@vertix.gg/base/src/interfaces/base-model-stub";
 
 export type TVersionType = `${ number }.${ number }.${ number }`;
@@ -40,12 +43,12 @@ export function DataVersioningModelFactory<
 
         public constructor(
             shouldDebugCache = ( undefined === typeof options.shouldDebugCache
-                ? isDebugEnabled( "CACHE", VersioningModel.getName() ) :
-                options.shouldDebugCache
+                    ? isDebugEnabled( "CACHE", VersioningModel.getName() ) :
+                    options.shouldDebugCache
             ),
             shouldDebugModel = ( undefined === typeof options.shouldDebugModel
-                ? isDebugEnabled( "MODEL", VersioningModel.getName() ) :
-                options.shouldDebugModel
+                    ? isDebugEnabled( "MODEL", VersioningModel.getName() ) :
+                    options.shouldDebugModel
             )
         ) {
             super( shouldDebugCache, shouldDebugModel );
@@ -136,13 +139,15 @@ export function DataVersioningModelFactory<
         public async update<T extends ReturnType<typeof this.getValueAsType>>( keys: TUniqueKeys, value: T ) {
             const dataType = this.getDataType( value );
 
+            const newValue = await this.mergeWithExisting( keys, value );
+
             const result = await this.getModel().update<TModelResult>( {
                 where: {
                     [ this.getUniqueKeyName() ]: keys
                 },
                 data: {
                     type: dataType,
-                    [ this.getValueField( dataType ) ]: this.transformValue( value, dataType )
+                    [ this.getValueField( dataType ) ]: this.transformValue( newValue, dataType )
                 }
             } );
 
@@ -163,7 +168,7 @@ export function DataVersioningModelFactory<
                 },
                 update: {
                     type: dataType,
-                    [ this.getValueField( dataType ) ]: this.transformValue( value, dataType )
+                    [ this.getValueField( dataType ) ]: this.transformValue( await this.mergeWithExisting( keys, value ), dataType )
                 },
                 create: {
                     ... keys,
@@ -177,6 +182,19 @@ export function DataVersioningModelFactory<
             this.deleteCacheIfAlreadyExists( keys );
 
             return result ? this.getValueAsType<T>( result ) : null;
+        }
+
+        private async mergeWithExisting( keys: TUniqueKeys, value: any ) {
+            let existing;
+
+            const dataType = this.getDataType( value );
+
+            if ( "object" === dataType ) {
+                existing = await this.get( keys );
+            }
+
+            // Deep merge existing object with the new value if it exists
+            return existing ? deepMerge( existing, value ) : value;
         }
 
         private setCacheResult( keys: TUniqueKeys, result: TModelResult ) {

7117a5ff3e5ad8063f5a0e93f2b8ce969260e3c1
Leonid Vinikov <czf.leo123@gmail.com>
2024-09-13 19:07:29 +0300

    New: (`vertix-base`) - Add strict data handling for `UserChannelDataModel`
    Introduced `ModelDataOwnerStrictDataBase` class to enforce strict data structure in `UserChannelDataModel`. Added `MasterChannelUserDataInterface` for detailed user configuration in channels. Implemented methods for managing master data and primary messages using strict data models.

---
+++ packages/vertix-base/src/interfaces/master-channel-user-config.ts
@@ -0,0 +1,15 @@
+import type { ChannelState, ChannelVisibilityState } from "@vertix.gg/bot/src/definitions/dynamic-channel";
+
+export interface MasterChannelUserDataInterface {
+    dynamicChannelName: string,
+    dynamicChannelUserLimit: number,
+    dynamicChannelState: ChannelState,
+    dynamicChannelVisibilityState: ChannelVisibilityState,
+    dynamicChannelAllowedUserIds: string[],
+    dynamicChannelBlockedUserIds: string[],
+    dynamicChannelRegion: string,
+    dynamicChannelPrimaryMessage: {
+        title?: string,
+        content?: string,
+    }
+}

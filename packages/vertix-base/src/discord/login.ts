27d8c9d1ce4a157661d86b1e32fc63a733ec1469
Leonid Vinikov <czf.leo123@gmail.com>
2026-09-04 22:14:25 +0300

    tweak: (`vertix-base`) - Extend Discord token fallback options
    Extend the Discord token fallback options to include `DISCORD_TEST_TOKEN` and `DISCORD_TOKEN` environment variables. This allows for more flexible token management and support in different environments.

--- packages/vertix-base/src/discord/login.ts
+++ packages/vertix-base/src/discord/login.ts
@@ -3,7 +3,7 @@ import process from "process";
 export let gToken = "";
 
 export default async function( client: any, onLogin: Function, token?: string ) {
-    const loginToken = ( token || process.env.DISCORD_BOT_TOKEN || "").trim();
+    const loginToken = ( token || process.env.DISCORD_BOT_TOKEN || process.env.DISCORD_TEST_TOKEN || process.env.DISCORD_TOKEN  || "").trim();
 
     if ( loginToken ) {
         // Only set the global token if it's the primary bot login (no explicit token provided)
